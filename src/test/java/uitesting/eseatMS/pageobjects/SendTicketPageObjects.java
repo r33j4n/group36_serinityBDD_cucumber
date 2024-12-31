48703524b6eced5ee608e2f073691b446e9aeafa
sagini18 <saginisaju@gmail.com>
2024-12-31 20:17:26 +0530

    update send ticket verification

--- src/test/java/uitesting/eseatMS/pageobjects/SendTicketPageObjects.java
+++ src/test/java/uitesting/eseatMS/pageobjects/SendTicketPageObjects.java
@@ -1,9 +1,10 @@
 package uitesting.eseatMS.pageobjects;
 
 import net.thucydides.core.pages.PageObject;
+import org.junit.Assert;
 
 public class SendTicketPageObjects extends PageObject {
     public void sendTicketPageVerified(){
-        System.out.println(getDriver().getTitle());
+        Assert.assertEquals(getDriver().getCurrentUrl(),"https://sltb.eseat.lk/ticket/send-view");
     }
 }
